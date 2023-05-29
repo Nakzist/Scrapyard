b700fe873fbfdc71add51fb671c6da1916a8759a
Kerem Aktuna <keremaktuna99@gmail.com>
2023-05-29 15:59:19 +0300

    Custom events for Observer Pattern

---
+++ Assets/[GAME]/Scripts/GlobalVariables/CustomEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace _GAME_.Scripts.GlobalVariables
+{
+    public static class CustomEvents
+    {
+        public const string OnEnemyDeath = "OnEnemyDeath";
+        public const string OnQueenDeath = "OnQueenDeath";
+        public const string IncreaseSCore = "IncreaseScore";
+    }
+}
