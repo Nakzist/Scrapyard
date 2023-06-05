aef36a7bd995754b2391f509649b7ae9689d4281
Kerem Aktuna <keremaktuna99@gmail.com>
2023-06-05 17:08:48 +0300

    Changed Node Structure

--- Assets/[GAME]/Scripts/Behaviour Tree/BtNode.cs
+++ Assets/[GAME]/Scripts/Behaviour Tree/BtNode.cs
@@ -4,6 +4,13 @@ namespace _GAME_.Scripts.Behaviour_Tree
 {
     public abstract class BtNode
     {
+        public BtNodeState CurrentState { get; protected set; }
+        
         public abstract BtNodeState Execute();
+
+        public virtual void Reset()
+        {
+            CurrentState = BtNodeState.Running;
+        }
     }
 }
