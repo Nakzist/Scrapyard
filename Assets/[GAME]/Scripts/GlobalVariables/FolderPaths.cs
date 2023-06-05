86e9545b82439713b20ebe1a78d810fe6ceaa4e3
Kerem Aktuna <keremaktuna99@gmail.com>
2023-06-05 22:18:07 +0300

    Implemented Pursuer Bot

--- Assets/[GAME]/Scripts/GlobalVariables/FolderPaths.cs
+++ Assets/[GAME]/Scripts/GlobalVariables/FolderPaths.cs
@@ -1,4 +1,5 @@
-﻿namespace _GAME_.Scripts.GlobalVariables
+﻿// ReSharper disable InconsistentNaming
+namespace _GAME_.Scripts.GlobalVariables
 {
     public static class FolderPaths
     {
@@ -7,8 +8,14 @@
         public const string HUD_DATA_PATH = "Player Data/Player Hud Data";
         
         public const string LEVEL_DATA_PATH = "Level Data/Level Data";
-
-        public const string SNIPER_BOT_DATA_PATH = "Enemy Data/Sniper Bot Data";
+        
+        public const string DRONE_BOT_DATA_PATH = "Enemy Data/Drone Bot Data";
+        public const string DUPLICO_BOT_DATA_PATH = "Enemy Data/Duplico Bot Data";
         public const string KAMIKAZE_BOT_DATA_PATH = "Enemy Data/Kamikaze Bot Data";
+        public const string PURSUER_BOT_DATA_PATH = "Enemy Data/Pursuer Bot Data";
+        public const string REGULAR_BOT_DATA_PATH = "Enemy Data/Regular Bot Data";
+        public const string SNIPER_BOT_DATA_PATH = "Enemy Data/Sniper Bot Data";
+        public const string SPINNER_BOT_DATA_PATH = "Enemy Data/Spinner Bot Data";
+        public const string VACUUM_BOT_DATA_PATH = "Enemy Data/Vacuum Bot Data";
     }
 }
